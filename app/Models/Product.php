a3b45b79073eb9c9f3331c88705726406053020b
hanfuyin <18910490095@163.com>
2018-10-17 16:09:24 +0000

    添加商品模型和后台管理

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'title', 'description', 'image', 'on_sale',
+        'rating', 'sold_count', 'review_count', 'price'
+    ];
+
+    protected $casts = [
+        'on_sale' => 'boolean'
+    ];
+
+    public function skus()
+    {
+        return $this->hasMany(ProductSku::class);
+    }
+}
